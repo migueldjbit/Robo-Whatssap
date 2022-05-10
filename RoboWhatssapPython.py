f18ccf757d6fa14f2017113a45a8118e81dc47a4
Miguel neto <djmiguelbit@gmail.com>
2022-05-10 14:58:07 -0300

    Nova alteração Para LGPD

--- RoboWhatssapPython.py
+++ RoboWhatssapPython.py
@@ -2,31 +2,41 @@ from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+import keyboard
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com/')
 time.sleep(20)
 
 
-contato = ['Junior Tati Tm','Ingrid']
-mensagem = ' Ola Tudo bem teste Robo'
+contato = ['jiih Silva','Debora Marcelo','Carlos Jose']
+
+#mensagem = ' Ola Tudo bem teste Robo'
 
 def buscar_contato(contato):
     campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-    time.sleep(3)
+    time.sleep(5)
     campo_pesquisa.click()
     campo_pesquisa.send_keys(contato)
     campo_pesquisa.send_keys(Keys.ENTER)
+    time.sleep(2)
+    keyboard.send("Ctrl+v")
+    keyboard.send("enter")
 
-def enviar_mensagem(mensagem):
-    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-    campo_mensagem[1].click()
-    time.sleep(3)
-    campo_mensagem[1].click()
-    campo_mensagem[1].send_keys(mensagem)
-    campo_mensagem[1].send_keys(Keys.ENTER)
+#def enviar_mensagem(mensagem):
+ #   campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+  #  campo_mensagem[1].click()
+   # time.sleep(3)
+    #campo_mensagem[1].click()
+    #campo_mensagem[1].send_keys(mensagem)
+    #campo_mensagem[1].send_keys(Keys.ENTER)
     
     
 for contato in contato:
     buscar_contato(contato)
-    enviar_mensagem(mensagem)
+    
+    #enviar_mensagem(mensagem)
+    
+    
+    
+    
